c15456bfedd3a3cdfa8e641e866857fb44ed5fff
Mitch <mitcht18@gmail.com>
2016-10-04 15:08:54 -0400

    Entering message in room logs on server - need to have it display in room

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
-
+app.get('/chatroom.html',function(req,res){
+    res.sendFile(__dirname + '/chatroom.html');
+});
 http.listen(5000,function(){
     console.log('listening on*:3000');
+});
+//Shows when a user connects to index.html
+io.on('connection',function(socket){
+    console.log('User Connected');
+    socket.on('chat message',function(msg){
+        console.log('message: ' + msg);
+    });
 });
