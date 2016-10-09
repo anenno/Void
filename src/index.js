c289bbdf67c30e3a9f7156db713fb5c66874db58
Orsayshi <or.khiyaev@gmail.com>
2016-10-09 19:46:08 -0400

    Server now tells us when user has disconnected.

--- src/index.js
+++ src/index.js
@@ -11,10 +11,17 @@ app.get('/chatroom.html',function(req,res){
 http.listen(5000,function(){
     console.log('listening on*:5000');
 });
-//Shows when a user connects to index.html
+//Shows when a user connects and disconnects to index.html
 io.on('connection',function(socket){
     console.log('User Connected');
-    socket.on('chat message',function(msg){
-        console.log('message: ' + msg);
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
     });
+});
+
+//Shows the chat messages.
+io.on('connection',function(socket){
+  socket.on('chat message',function(msg){
+    console.log('message: ' + msg);
+  });
 });
