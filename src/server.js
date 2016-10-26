8e4a3b392aab8aac5f180b949f99f2f02081e284
mitch <mitcht18@gmail.com>
2016-10-26 11:59:58 -0400

    Started work on chatrooms and alias support

--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ app.get('/chatroom.html',function(req,res){
 http.listen(5000,function(){
     console.log('listening on*:5000');
 });
+
+
+
+
+/*
 //Shows when a user connects and disconnects to index.html
 io.on('connection',function(socket){
     console.log('User Connected');
@@ -18,6 +23,39 @@ io.on('connection',function(socket){
       console.log('user disconnected');
     });
 });
+*/
+
+var users = {};
+var chatrooms = {};
+
+io.sockets.on('connection',function(socket){
+
+    //When client sends 'createalias'
+    //Add alias to clients socket
+    socket.on('createalias',function(alias){
+        socket.alias = alias;
+    }
+
+    //Client sends 'joinroom'
+    socket.on('joinroom',function(roomname){
+        socket.join(roomname);
+        socket.room.users[username] = socket.alias;
+
+
+    }
+
+    // When client sends 'sendmsg'
+    // Update the chat with username and the msg
+    socket.on('sendmsg',function(data){
+        io.sockets.emit('updatechat',socket.username,data);
+    });
+
+    socket.on('joinroom',function(data){
+        socket.join(data)
+    })
+
+
+}
 
 //Shows the chat messages.
 io.on('connection',function(socket){
