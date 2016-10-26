3181927b201b6d35db82dd58534d74d6b63ed848
Mitch <mitcht18@gmail.com>
2016-10-26 13:57:00 -0400

    continued work on functionality of rooms and user id's

--- src/server.js
+++ src/server.js
@@ -2,48 +2,50 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//Send Initial page when client connects to homepage
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/chatroom.html',function(req,res){
-    res.sendFile(__dirname + '/chatroom.html');
-});
+
+//Send chatroom.html when client emits enterRoom
+
+
 http.listen(5000,function(){
     console.log('listening on*:5000');
 });
 
-
-
-
-/*
-//Shows when a user connects and disconnects to index.html
-io.on('connection',function(socket){
-    console.log('User Connected');
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
-});
-*/
-
 var users = {};
-var chatrooms = {};
+var chatrooms = [];
 
 io.sockets.on('connection',function(socket){
+
+    socket.on('enterRoom',function(socket){
+        app.get('/chatroom.html',function(req,res){
+            res.sendFile(__dirname + '/chatroom.html');
+        });
+    });
 
     //When client sends 'createalias'
     //Add alias to clients socket
     socket.on('createalias',function(alias){
         socket.alias = alias;
-    }
+        //users[alias] = alias;
+        //Log that a new alias has been created
+        console.log('SERVER: New Alias Created - ' + socket.alias);
+
+    });
 
     //Client sends 'joinroom'
     socket.on('joinroom',function(roomname){
         socket.join(roomname);
-        socket.room.users[username] = socket.alias;
+        chatrooms[roomname] = roomname;
 
+        //Log that a room has been created
+        console.log('SERVER: New Room Created - ' + roomname);
+        listrooms(chatrooms);
+    });
 
-    }
-
+    /*
     // When client sends 'sendmsg'
     // Update the chat with username and the msg
     socket.on('sendmsg',function(data){
@@ -53,13 +55,22 @@ io.sockets.on('connection',function(socket){
     socket.on('joinroom',function(data){
         socket.join(data)
     })
+    */
 
 
-}
 
+});
 //Shows the chat messages.
 io.on('connection',function(socket){
   socket.on('chat message',function(msg){
     console.log('message: ' + msg);
   });
 });
+
+//Helper Functions
+function listrooms(chatrooms){
+    console.log('SERVER: Current Active Rooms');
+    for( i = 1; i < chatrooms.length; i++){
+        console.log(chatrooms[i]);
+    }
+}
