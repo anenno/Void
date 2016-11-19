07223f79d8f53a0ca69a7ce0efa078e6a8b074bd
Mitch <mitcht18@gmail.com>
2016-11-19 15:46:58 -0500

    Chat works with one room. Updates Div as well. Working on Multi room now.

--- server.js
+++ server.js
@@ -6,68 +6,52 @@ var PORT = 5000;
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//var path  = require('path');
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
+
 app.get('/chatroom.html',function(req,res){
     res.sendFile(__dirname + '/chatroom.html');
 });
 
-io.on('connection',function(socket){
-    socket.on('updateChat',function(from,msg){
-        io.emit('updateChat',from,msg);
-    });
-    socket.on('joinRoom',function(user){
-        io.emit('joinRoom',user);
-    });
-});
 //Listener
 http.listen(PORT,function(){
     console.log('listening on*:'+PORT);
 });
-//###################################################
 
-
-
-/*#################################################
-                OLD SERVER CODE                   #
-##################################################
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var PORT = 5000;
-
-//Send Initial page when client connects to homepage
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/index.html');
+io.on('connection', function(socket){
+    socket.on('sendmsg', function(msg){
+        console.log(msg);
+        io.emit('updateChat',msg);
+    });
+    socket.on('joinRoom',function(msg){
+        console.log("User Connected");
+    });
 });
-http.listen(PORT,function(){
-    console.log('listening on*:'+PORT);
-});
-app.get('/chatroom.html',function(req,res){
-    res.sendFile(__dirname + '/chatroom.html');
-});
-//###################################################
 
+/*
+###################OLD CODE####################
+###############################################
+MultiRoom Chat - NOT WORKING COOL
 
-
-
-
-
-//All active chat room names saved here
-var chatrooms = [];
 
 //All active users in the current room
+var chatrooms = [];
 io.sockets.on('connection',function(socket){
+
+
+
     //Client emits 'createalias'
-    socket.on('createalias',function(alias){
+    socket.on('createalias',function(alias) {
         socket.alias = alias;
         //Log that a new alias has been created
         console.log('server: New Alias Created - ' + socket.alias);
-
+        socket.emit('consoleLog');
     });
+
+
+
 
     //Client emits 'joinroom'
     socket.on('joinroom',function(roomname){
@@ -84,7 +68,7 @@ io.sockets.on('connection',function(socket){
         //Log that a room has been created or that a user has a joined a specific room.
         var flag = roomexists(chatrooms,roomname);
         if(flag == true){
-            console.log('server: User:  ' + socket.alias + "joined " + socket.room);
+            console.log('server: User:  ' + socket.alias + " joined " + socket.room);
             listrooms(chatrooms);
         }else{
             console.log('server: New Room Created - ' + socket.room);
@@ -94,17 +78,13 @@ io.sockets.on('connection',function(socket){
     });
 
     //Client emits 'sendmsg'
-    //This will emit 'updateChat' to client to update chatroom.html
     socket.on('sendmsg',function(data){
         io.sockets.in(socket.room).emit('updateChat',socket.alias,data);
-        //Logs message on console
-        //Alias and Room ID are undefined , I'm not sure why but they are most likely undefined above
-        //Maybe they need to be passed from client
-        console.log("Room ID: " + socket.room + "Alias: " + socket.alias + " " + data);
-
-    });
-
+        console.log("Message Received: " + data);
+        });
 });
+ ###############################################
+*/
 
 //Helper Functions
 function listrooms(chatrooms){
@@ -121,5 +101,5 @@ function roomexists(chatrooms,roomname){
     }
     return false;
 }
- */
+
 
