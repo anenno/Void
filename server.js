5ed8a41349e44dd68e054e84295426f66bce157a
Mitch <mitcht18@gmail.com>
2016-11-19 18:12:21 -0500

    Chat only working with multiple static rooms. Usernames are not working

--- server.js
+++ server.js
@@ -14,21 +14,74 @@ app.get('/',function(req,res){
 app.get('/chatroom.html',function(req,res){
     res.sendFile(__dirname + '/chatroom.html');
 });
-
+app.get('/room1.html',function(req,res){
+    res.sendFile(__dirname + '/room1.html');
+});
+app.get('/room2.html',function(req,res){
+    res.sendFile(__dirname + '/room2.html');
+});
+app.get('/room3.html',function(req,res){
+    res.sendFile(__dirname + '/room3.html');
+});
 //Listener
 http.listen(PORT,function(){
     console.log('listening on*:'+PORT);
 });
 
-io.on('connection', function(socket){
+
+io
+.of('/room1')
+.on('connection', function(socket){
+    console.log("User connected to Room1");
     socket.on('sendmsg', function(msg){
-        console.log(msg);
-        io.emit('updateChat',msg);
+        console.log(socket.alias + ":" + msg);
+        io.of('/room1').emit('updateChat',msg,socket.alias);
     });
-    socket.on('joinRoom',function(msg){
-        console.log("User Connected");
+    socket.on('joinRoom',function(alias,roomname){
+        app.get('/room1.html',function(req,res){
+            res.sendFile(__dirname + '/room1.html');
+        });
+        socket.room = roomname;
+        socket.alias = alias;
+        console.log(socket.alias + " joined " + socket.room);
+    });
+    socket.on('disconnect',function(){
     });
 });
+io
+    .of('/room2')
+    .on('connection', function(socket){
+        console.log("User connected to Room2");
+
+        socket.on('sendmsg', function(msg){
+            console.log(socket.alias + ":" + msg);
+            io.of('/room2').emit('updateChat',msg,socket.alias);
+        });
+        socket.on('joinRoom',function(alias,roomname){
+            socket.room = roomname;
+            socket.alias = alias;
+            console.log(socket.alias + " joined " + socket.room);
+        });
+        socket.on('disconnect',function(){
+        });
+    });
+io
+    .of('/room3')
+    .on('connection', function(socket){
+        console.log("User connected to Room3");
+
+        socket.on('sendmsg', function(msg){
+            console.log(socket.alias + ":" + msg);
+            io.of('/room3').emit('updateChat',msg,socket.alias);
+        });
+        socket.on('joinRoom',function(alias,roomname){
+            socket.room = roomname;
+            socket.alias = alias;
+            console.log(socket.alias + " joined " + socket.room);
+        });
+        socket.on('disconnect',function(){
+        });
+    });
 
 /*
 ###################OLD CODE####################
