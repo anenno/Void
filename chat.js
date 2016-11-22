22a71406048909e2322159163e81cb24e070474f
Mitch <mitcht18@gmail.com>
2016-11-22 17:24:43 -0500

    Continued work on username/room checks. Almost finished.

--- chat.js
+++ chat.js
@@ -1,62 +1,110 @@
 /**
  * Created by mitch on 11/22/16.
  */
-var user = {
-    socket:null,
-    alias:"",
-    room:""
-};
-var room = {
-    connectedUsers: [],
-    name: "",
-    password:"",
-    admin:null,
-    roomCreator:null
-};
-var rooms = [];
+/*
+    Module Exports
+ */
+var chatrooms = [];
 
-function getRoom(roomname){
-    return rooms[roomname];
-}
-function getUser(roomname,username){
-    var room = getRoom(roomname);
-    return room.connectedUsers[username];
-}
-function setUser(user,socket,alias,room){
-    user.socket = socket;
-    user.alias = alias;
-    user.room = room;
-    addUserToRoom(user,room);
-}
-function setRoom(room,name){
-    room.name = name;
+module.exports = function() {
 
-}
-function addUserToRoom(user,room){
-    room.connectedUsers.push(user);
-}
-function removeUserFromRoom(username,room){
-    var index = room.connectedUsers.indexOf(username);
-    if(index > -1){
+    this.User = function(socket,alias,roomname){
+        this.socket = socket;
+        this.alias = alias;
+        this.roomname = roomname;
+        this.room = chatrooms[roomname];
+
+    };
+    this.Room = function(name,password,admin,roomCreator){
+        this.connectedUsers = [];
+        this.numberOfUsers = 0;
+        this.name = name;
+        this.password = password;
+        this.admin = admin;
+        this.roomCreator = roomCreator;
+        this.maxUsers = -1;
+    };
+
+    this.getRoom = function(roomname){
+        return chatrooms[roomname];
+    };
+    this.setRoom = function(roomname,room){
+        chatrooms[roomname] = room;
+    }
+    this.getUser = function(room,username){
+        return room.connectedUsers[username];
+    };
+
+    this.addUserToRoom = function(user,roomname){
+        var key = user.alias;
+        var room = this.getRoom(roomname);
+        room.connectedUsers[key] = user;
+        room.numberOfUsers = room.numberOfUsers + 1;
+
+        //Update room in room array
+        this.setRoom(room.name,room);
+        console.log("Add User To Room, number of users = " + room.numberOfUsers);
+    };
+    this.removeUserFromRoom = function(username,roomname){
+        var room = this.getRoom(roomname);
+        var index = room.connectedUsers.indexOf(username);
+        console.log("Number of users in room = " + this.getRoom(roomname).numberOfUsers);
+        console.log("Removing " + room.connectedUsers[username].alias + " from room - " + room.name);
+
         room.connectedUsers.splice(index,1);
-    }
-}
-function isNameAvailable(roomname,username){
-    var room = getRoom(roomname);
-    if(room.connectedUsers.indexOf[username] == -1){
-        //Username does not exist return true
-        return true;
-    }else{
-        //Username exists return false
-        return false;
-    }
-}
-function doesRoomExist(roomname){
-    if(rooms.indexOf(roomname) == -1){
-        //Room does not exist yet
-        return false;
-    }else{
-        //Room already exists
-        return true;
-    }
-}
+        room.numberOfUsers = room.numberOfUsers - 1;
+
+        for(var i in room.connectedUsers){
+            console.log(room.connectedUsers[i].alias);
+        }
+
+        //Update room in chatrooms array
+        this.getRoom(roomname).connectedUsers = room.connectedUsers;
+        console.log("Number of users in room = " + this.getRoom(roomname).numberOfUsers);
+        //this.getRoom(roomname).numberOfUsers =
+
+
+    };
+    this.deleteRoom = function(roomname){
+        var index = chatrooms.indexOf(roomname);
+        if(index > -1){
+            chatrooms.splice(index,1);
+        }
+    };
+    this.isNameAvailable = function(roomname,username){
+        //Check if room exists
+        //If it does then check if name exists
+        if(this.roomExists(roomname) == true){
+            var room = this.getRoom(roomname);
+            var connectedUsers = room.connectedUsers;
+            if(username in connectedUsers == false){
+                //Username does not exist return true
+                return true;
+            }else {
+                //Username exists return false
+                return false;
+            }
+        //If room does not exist yet just return true
+        //Room creation is done in server file
+        }else{
+            return true;
+        }
+    };
+
+    this.roomExists = function(roomname) {
+        if(roomname in chatrooms == true){
+            return true;
+        }else{
+            return false;
+        }
+    };
+    this.updateChatRooms = function(room) {
+        var key = room.name;
+        chatrooms[key] = room;
+    };
+};
+
+
+
+
+
