9aa5e97689227ddf5679164931230df42a933bcd
mitch <mitcht18@gmail.com>
2016-12-07 22:26:55 -0500

    working on command line args

--- server.js
+++ server.js
@@ -7,13 +7,53 @@
  */
 require('./chat.js')();
 
-var PORT = 5000;
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var striptags = require('striptags');
 var cryptico = require('cryptico');
+
+var PORT = 5000;
+var debug = 0;
+
+if(process.argv.length == 2) {
+    /*
+     Start server with no args
+     Default Port - 5000
+     */
+
+    http.listen(PORT, function () {
+        console.log('listening on*:' + PORT);
+    });
+}
+if(process.argv.length == 3){
+    /*
+     Set PORT value to passed parameter - if it is valid
+     */
+    if(process.argv[2] >= 0 || process.argv[2]<= 65535) {
+        PORT = process.argv[2];
+
+        /*
+         Listeners
+         */
+        http.listen(PORT, function () {
+            console.log('listening on*:' + PORT);
+        });
+    }else{
+        console.log('proper usage: node server.js PORTNUMBER');
+        console.log('              node server.js');
+
+    }
+}else{
+    console.log('proper usage: node server.js PORTNUMBER');
+    console.log('              node server.js');
+}
+
+
+
+
 /*
     Express Routes
  */
@@ -28,12 +68,7 @@ app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
-/*
-    Listeners
- */
-http.listen(PORT,function(){
-    console.log('listening on*:'+PORT);
-});
+
 
 /*
     Socket.IO Functions
